0a9d1654d74d0134320ce6425c2689c8d9327249
salemaljebaly <salemaljebaly@gmail.com>
2025-06-04 08:40:21 +0200

    refactor: simplify ResizeObserver callback in AnimatedBeam component

--- src/components/magicui/animated-beam.tsx
+++ src/components/magicui/animated-beam.tsx
@@ -92,11 +92,9 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
     };
 
     // Initialize ResizeObserver
-    const resizeObserver = new ResizeObserver((entries) => {
+    const resizeObserver = new ResizeObserver(() => {
       // For all entries, recalculate the path
-      for (let entry of entries) {
-        updatePath();
-      }
+      updatePath();
     });
 
     // Observe the container element
@@ -130,7 +128,7 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
       xmlns="http://www.w3.org/2000/svg"
       className={cn(
         "pointer-events-none absolute left-0 top-0 transform-gpu stroke-2",
-        className,
+        className
       )}
       viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}
     >
