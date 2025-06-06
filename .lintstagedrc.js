c3ac4503e026e506e41836c1981effb1b604cb88
salemaljebaly <salemaljebaly@gmail.com>
2025-06-06 17:26:07 +0200

    chore: remove lint-staged configuration file

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
-  '*.css': ['prettier --write'],
-  'package.json': ['npx sort-package-json'],
-}
